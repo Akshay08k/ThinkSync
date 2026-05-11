0fd4fbe4b9271a354f0a2951d1e2901b391e4dda
akshay <akshaykomade012345@gmail.com>
2026-05-11 22:26:56 +0530

    added warning message

--- thinkSyncFE/src/main.jsx
+++ thinkSyncFE/src/main.jsx
@@ -9,7 +9,12 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DarkModeProvider>
       <AuthProvider>
-        <App />
+        <h1 className="text-3xl font-bold underline">
+          Hello Who Are Visiting This Site From That Spam Mail
+          Please Avoid Clicking On Any Link In That Mail Because 
+          It was my resendAPI key which got hacked from somewhere
+          and mails were sended from that account 
+          Please avoid those mails</h1> 
       </AuthProvider>
     </DarkModeProvider>
   </StrictMode>
